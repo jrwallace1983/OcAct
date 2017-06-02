3ec5f4b17d7199f4c7ce6eb8d3252d0090c6543f
jwallace <jrwallace1983@gmail.com>
2017-06-01 23:11:36 -0700

    js cleanup of webmap script

--- webmap/script/archive/script - Copy(4).js
+++ webmap/script/archive/script - Copy(4).js
@@ -1,3 +1,5 @@
+      var map;
+
       require(["esri/map",
 	  "esri/dijit/Scalebar",
 	  "esri/dijit/LocateButton",
@@ -24,56 +26,42 @@
 	  function(Map, Scalebar, LocateButton,
 	  Search, ArcGISDynamicMapServiceLayer, FeatureLayer, InfoTemplate, Legend,
 	  arrayUtils, BasemapGallery, arcgisUtils,webMercatorUtils, on, Query, QueryTask, SimpleMarkerSymbol, Color, SpatialReference, dom, parser) {
-		  //declare variables
-		  var map,
-		  sr,
-		  scalebar,
-		  geoLocate,
-		  search,
-		  basemapGallery,
-		  template,
-		  symbol,
-		  symbol2,
-		  dynamicMapServiceLayer,
-		  hydrant_string,
-		  sysvalve_string,
-		  hydrantLayer,
-		  sysvalveLayer,
-		  query,
-		  valveclick,
-		  hydrantclick;
-		  		
-		  //This is the new map object
-		  map = new Map("map", {
+		//The parser function allows the dojo to be configured within the html node
+		parser.parse();
+		
+		//This is the new map object
+        var map = new Map("map", {
           basemap: "streets",  //For full list of pre-defined basemaps, navigate to http://arcg.is/1JVo6Wd
           center: [-88.158, 41.757], // longitude, latitude
           zoom: 13,
-          });
-		  
-		  //Spatial reference for query otherwise this will be projected incorrectly
-		  sr = new SpatialReference(102100);
+        });
+		//Spatial reference for query otherwise this will be projected incorrectly
+		var sr = new SpatialReference(102100);
 
+		
 		  //This is the scalebar object
-		  scalebar = new Scalebar({
+		  var scalebar = new Scalebar({
           map: map,
           // "dual" displays both miles and kilometers
           // "english" is the default, which displays miles
           // use "metric" for kilometers
           scalebarUnit: "english"
-          });
+        });
 		
 		//This is the geolocate object
-		geoLocate = new LocateButton({
+		var geoLocate = new LocateButton({
 		map: map
 		}, "LocateButton");
+		geoLocate.startup();
 		
 		//This is the address locator object
-		search = new Search({
+		var search = new Search({
         map: map
         }, "search");
-
+		search.startup();
+		
 		//This is the basemap Gallery object
-		basemapGallery = new BasemapGallery({
+		var basemapGallery = new BasemapGallery({
         showArcGISBasemaps: true,
         map: map
       }, "basemapGallery");
@@ -81,33 +69,55 @@
 		basemapGallery.on("error", function(msg) {
         console.log("basemap gallery error:  ", msg);
       });
+		basemapGallery.startup();
 	  
+
 		// This is the InfoTemplate popup object
-		template = new InfoTemplate("Attributes", "${*}");
+		var template = new InfoTemplate("Attributes", "${*}");
 
 		  //create symbol for selected features
 		symbol = new SimpleMarkerSymbol();
+		symbol.setStyle(SimpleMarkerSymbol.STYLE_CIRCLE);
+		symbol.setSize(25);
+		symbol.setColor(new Color([200,255,0,0.5]));
 		
 		symbol2 = new SimpleMarkerSymbol();
+		symbol2.setStyle(SimpleMarkerSymbol.STYLE_CIRCLE);
+		symbol2.setSize(25);
+		symbol2.setColor(new Color([251,104,105,0.5]));
+
 
 		//This is the dynamic Map Service layer objects
-		dynamicMapServiceLayer = new ArcGISDynamicMapServiceLayer(
+		var dynamicMapServiceLayer = new ArcGISDynamicMapServiceLayer(
 		"https://sampleserver6.arcgisonline.com/arcgis/rest/services/Water_Network/MapServer", {
 		});
-	    hydrant_string = "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Water_Network/FeatureServer/8"
-		sysvalve_string = "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Water_Network/FeatureServer/15"
+	    var hydrant_string = "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Water_Network/FeatureServer/8"
+		var sysvalve_string = "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Water_Network/FeatureServer/15"
 		//This is the feature layer object
-		hydrantLayer = new FeatureLayer(hydrant_string,{
+		var hydrantLayer = new FeatureLayer(hydrant_string,{
 			mode: FeatureLayer.MODE_SNAPSHOT,
             infoTemplate: template,
             outFields: ["*"]
 		});
-		sysvalveLayer = new FeatureLayer(sysvalve_string,{
+		var sysvalveLayer = new FeatureLayer(sysvalve_string,{
 			mode: FeatureLayer.MODE_SNAPSHOT,
             infoTemplate: template,
             outFields: ["*"]
 		});
-        query = new Query();
+		map.addLayers([hydrantLayer,sysvalveLayer, dynamicMapServiceLayer]);
+		
+		//This is the query task and query object
+		//var hydrantqueryTask = new QueryTask("https://sampleserver6.arcgisonline.com/arcgis/rest/services/Water_Network/FeatureServer/8");
+		//var valvequeryTask = new QueryTask("https://sampleserver6.arcgisonline.com/arcgis/rest/services/Water_Network/FeatureServer/15");
+
+        var query = new Query();
+        query.returnGeometry = true;
+        query.outFields = ["*"];
+		query.outSpatialReference = sr;
+		 
+		 // These are the query handlers
+		var valveclick = on(dom.byId("execute-query-valve"), "click", execute)
+		var hydrantclick = on(dom.byId("execute-query-hydrant"), "click", execute);
 		
 		function execute (evt) {
 			var asset,
@@ -122,7 +132,7 @@
 				asset = "hydrant";
 				task = new QueryTask(hydrant_string);
 				symboltype = symbol2;
-			};
+			}
 			console.log(evt.currentTarget.id)
 			
 			//if you use query.text that looks for display field the query where you can write the query. Notice single quotes for the string fields
@@ -148,11 +158,11 @@
 					for (var attr in featureAttributes) {
 					if(attr == "facilityid"){
 					resultItems.push("<b>" + attr + ":</b>  " + featureAttributes[attr] + "<br>")};
-					};
+					}
 					resultItems.push("<br>");
-				};
+				}
 				dom.byId("queryinfo-" + asset).innerHTML = resultItems.join("");
-			};
+			}
 		
 			function showResults2 (results) {
 				map.graphics.clear();
@@ -174,7 +184,7 @@
 				}
 				dom.byId("queryinfo-" + asset).innerHTML = resultItems.join("");
 			}
-		};
+		}
 	
 		//add the legend
 		map.on("layers-add-result", function (evt) {
@@ -187,13 +197,14 @@
             layerInfos: layerInfo
           }, "legendDiv");
           legendDijit.startup();
-		};
+		}
 		});
 		  map.on("load", function() {
           //after map loads, connect to listen to mouse move & drag events
           map.on("mouse-move", showCoordinates);
           map.on("mouse-drag", showCoordinates);
         });
+
 		//Function to  show the coordinates
         function showCoordinates(evt) {
           //the map is in web mercator but display coordinates in geographic (lat, long)
@@ -201,30 +212,4 @@
           //display mouse coordinates
           dom.byId("info").innerHTML = mp.x.toFixed(6) + ", " + mp.y.toFixed(6);
         }
-		//methods to start geolocator, search, basemapGallery, and add layers
-		geoLocate.startup();
-		search.startup();
-		basemapGallery.startup();
-		map.addLayers([hydrantLayer,sysvalveLayer, dynamicMapServiceLayer]);
-		
-		//Set default symbol properties
-		symbol.setStyle(SimpleMarkerSymbol.STYLE_CIRCLE);
-		symbol.setSize(25);
-		symbol.setColor(new Color([200,255,0,0.5]));
-		
-		symbol2.setStyle(SimpleMarkerSymbol.STYLE_CIRCLE);
-		symbol2.setSize(25);
-		symbol2.setColor(new Color([251,104,105,0.5]));
-		
-		//Set default query properties
-		query.returnGeometry = true;
-        query.outFields = ["*"];
-		query.outSpatialReference = sr;
-		
-		// These are the query handlers
-		valveclick = on(dom.byId("execute-query-valve"), "click", execute)
-		hydrantclick = on(dom.byId("execute-query-hydrant"), "click", execute);
-		
-		//The parser function allows the dojo to be configured within the html node
-		parser.parse();
       });
